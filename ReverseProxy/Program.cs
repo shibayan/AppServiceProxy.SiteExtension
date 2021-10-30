822fc5c6bb3f51a135262969c2af8a3c93eaa485
Tatsuro Shibamura <me@shibayan.jp>
2021-10-31 01:51:40 +0900

    Adding initial project

---
+++ ReverseProxy/Program.cs
@@ -0,0 +1,15 @@
+using ReverseProxy;
+
+using Yarp.ReverseProxy.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy();
+
+builder.Services.AddSingleton<IProxyConfigProvider, ProxiesJsonFileConfigProvider>();
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
---
+++ ReverseProxy/ProxiesJsonFileConfigProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Primitives;
+
+using Yarp.ReverseProxy.Configuration;
+
+namespace ReverseProxy
+{
+    public class ProxiesJsonFileConfigProvider : IProxyConfigProvider
+    {
+        public ProxiesJsonFileConfigProvider()
+        {
+            var wwwroot = Environment.ExpandEnvironmentVariables(@"%HOME%\site\wwwroot");
+            var proxiesJsonFile = Path.Combine(wwwroot, ProxiesJsonFileName);
+
+            _config = new ProxiesJsonFileConfig(proxiesJsonFile);
+        }
+
+        private const string ProxiesJsonFileName = "proxies.json";
+
+        private readonly ProxiesJsonFileConfig _config;
+
+        public IProxyConfig GetConfig() => _config;
+
+        private class ProxiesJsonFileConfig : IProxyConfig
+        {
+            public ProxiesJsonFileConfig(string proxiesJsonFile)
+            {
+                var json = File.ReadAllText(proxiesJsonFile);
+            }
+
+            public IReadOnlyList<RouteConfig> Routes { get; }
+
+            public IReadOnlyList<ClusterConfig> Clusters { get; }
+
+            public IChangeToken ChangeToken { get; }
+        }
+    }
+}
