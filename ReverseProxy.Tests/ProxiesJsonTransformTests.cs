4fb24b9c9f49ffcb1e3e25678113241eb6ace8f1
Tatsuro Shibamura <me@shibayan.jp>
2021-11-12 19:48:18 +0900

    Fixed Null Reference Type warning

--- ReverseProxy.Tests/ProxiesJsonTransformTests.cs
+++ ReverseProxy.Tests/ProxiesJsonTransformTests.cs
@@ -38,6 +38,10 @@ namespace ReverseProxy.Tests
             Assert.Equal($"Cluster_{proxies[0].Name}", clusters[0].ClusterId);
             Assert.Equal(proxies[0].MatchCondition.Methods, routes[0].Match.Methods);
             Assert.Equal(proxies[0].MatchCondition.Route, routes[0].Match.Path);
+
+            Assert.NotNull(routes[0].Transforms);
+            Assert.Equal(1, routes[0].Transforms.Count);
+
             Assert.Equal("/backend/{test}", routes[0].Transforms[0]["PathPattern"]);
             Assert.Equal("https://<AnotherApp>.azurewebsites.net", clusters[0].Destinations[$"Cluster_{proxies[0].Name}/destination"].Address);
         }
@@ -95,6 +99,8 @@ namespace ReverseProxy.Tests
 
             Assert.Equal(1, routes.Count);
             Assert.Equal(1, clusters.Count);
+
+            Assert.NotNull(routes[0].Transforms);
             Assert.Equal(2, routes[0].Transforms.Count);
 
             Assert.Equal("/backend/{test}", routes[0].Transforms[0]["PathPattern"]);
@@ -132,7 +138,9 @@ namespace ReverseProxy.Tests
             Assert.Equal(1, routes.Count);
             Assert.Equal(1, clusters.Count);
 
+            Assert.NotNull(routes[0].Transforms);
             Assert.Equal(2, routes[0].Transforms.Count);
+
             Assert.Equal("/backend/{test}", routes[0].Transforms[0]["PathPattern"]);
             Assert.Equal("Content-Type", routes[0].Transforms[1]["ResponseHeader"]);
             Assert.Equal("text/plain", routes[0].Transforms[1]["Append"]);
@@ -164,7 +172,10 @@ namespace ReverseProxy.Tests
             Assert.Equal(1, clusters.Count);
 
             Assert.Equal("/{**path}", routes[0].Match.Path);
+
+            Assert.NotNull(routes[0].Transforms);
             Assert.Equal(1, routes[0].Transforms.Count);
+
             Assert.Equal("/backend/{**path}", routes[0].Transforms[0]["PathPattern"]);
         }
 
@@ -198,11 +209,17 @@ namespace ReverseProxy.Tests
 
             Assert.Equal(1, routes.Count);
             Assert.Equal(1, clusters.Count);
+
+            Assert.NotNull(routes[0].Transforms);
             Assert.Equal(2, routes[0].Transforms.Count);
 
             Assert.Equal("/backend/{test}", routes[0].Transforms[0]["PathPattern"]);
             Assert.Equal("x-functions-key", routes[0].Transforms[1]["RequestHeader"]);
             Assert.Equal("api-key", routes[0].Transforms[1]["Append"]);
+
+            Assert.NotNull(clusters[0].Destinations);
+            Assert.Equal(1, clusters[0].Destinations.Count);
+
             Assert.Equal("https://example.com", clusters[0].Destinations[$"Cluster_{proxies[0].Name}/destination"].Address);
         }
     }
