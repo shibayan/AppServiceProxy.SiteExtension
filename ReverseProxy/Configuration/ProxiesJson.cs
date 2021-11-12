8461de25cbf9c6ee7be031284b1db6ff9acf85f3
Tatsuro Shibamura <me@shibayan.jp>
2021-11-12 19:49:24 +0900

    Adding build workflow (#3)
    * Adding build workflow
    
    * Skip failed test case
    
    * Fixed Null Reference Type warning

--- ReverseProxy/Configuration/ProxiesJson.cs
+++ ReverseProxy/Configuration/ProxiesJson.cs
@@ -4,13 +4,13 @@
     {
         internal class ProxyConfig
         {
-            public string Name { get; set; }
+            public string Name { get; init; } = null!;
 
             public bool Disabled { get; init; }
 
-            public MatchConditionConfig MatchCondition { get; init; }
+            public MatchConditionConfig MatchCondition { get; init; } = null!;
 
-            public string BackendUri { get; init; }
+            public string BackendUri { get; init; } = null!;
 
             public RequestOverridesConfig? RequestOverrides { get; init; }
 
@@ -19,23 +19,23 @@
 
         internal class MatchConditionConfig
         {
-            public IReadOnlyList<string> Methods { get; init; }
+            public IReadOnlyList<string> Methods { get; init; } = null!;
 
-            public string Route { get; init; }
+            public string Route { get; init; } = null!;
         }
 
         internal class RequestOverridesConfig
         {
             public string? Method { get; init; }
-            public IReadOnlyDictionary<string, string> QueryString { get; init; }
-            public IReadOnlyDictionary<string, string> Headers { get; init; }
+            public IReadOnlyDictionary<string, string> QueryString { get; init; } = null!;
+            public IReadOnlyDictionary<string, string> Headers { get; init; } = null!;
         }
 
         internal class ResponseOverridesConfig
         {
             public int? StatusCode { get; init; }
             public string? StatusReason { get; init; }
-            public IReadOnlyDictionary<string, string> Headers { get; init; }
+            public IReadOnlyDictionary<string, string> Headers { get; init; } = null!;
         }
     }
 }
