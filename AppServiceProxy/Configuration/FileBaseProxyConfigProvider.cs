1259d304c10a6e3fdc386b4baf404f2b11b9f8aa
Tatsuro Shibamura <me@shibayan.jp>
2022-01-23 19:21:22 +0900

    Adding file-based proxy config provider

---
+++ AppServiceProxy/Configuration/FileBaseProxyConfigProvider.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+using Yarp.ReverseProxy.Configuration;
+
+namespace AppServiceProxy.Configuration
+{
+    internal class FileBaseProxyConfigProvider : IProxyConfigProvider
+    {
+        public FileBaseProxyConfigProvider(IEnumerable<IConfigFileLoader> configFileLoaders)
+        {
+            _configFileLoaders = configFileLoaders;
+
+            _fileProvider = new PhysicalFileProvider(_wwwroot)
+            {
+                UseActivePolling = true,
+                UsePollingFileWatcher = true
+            };
+        }
+
+        private readonly IEnumerable<IConfigFileLoader> _configFileLoaders;
+        private readonly PhysicalFileProvider _fileProvider;
+
+        private static readonly string _wwwroot = Environment.ExpandEnvironmentVariables(@"%HOME%\site\wwwroot");
+
+        public IProxyConfig GetConfig()
+        {
+            var configFileLoader = _configFileLoaders.FirstOrDefault(x => File.Exists(Path.Combine(_wwwroot, x.ConfigFileName)));
+
+            if (configFileLoader is null)
+            {
+                return new FileBaseProxyConfig
+                {
+                    Routes = Array.Empty<RouteConfig>(),
+                    Clusters = Array.Empty<ClusterConfig>(),
+                    ChangeToken = NullChangeToken.Singleton
+                };
+            }
+
+            var contents = File.ReadAllText(Path.Combine(_wwwroot, configFileLoader.ConfigFileName));
+
+            var (routes, clusters) = configFileLoader.LoadConfig(contents);
+
+            var changeToken = _fileProvider.Watch(configFileLoader.ConfigFileName);
+
+            return new FileBaseProxyConfig
+            {
+                Routes = routes,
+                Clusters = clusters,
+                ChangeToken = changeToken
+            };
+        }
+
+        private class FileBaseProxyConfig : IProxyConfig
+        {
+            public IReadOnlyList<RouteConfig> Routes { get; init; } = null!;
+
+            public IReadOnlyList<ClusterConfig> Clusters { get; init; } = null!;
+
+            public IChangeToken ChangeToken { get; init; } = null!;
+        }
+    }
+}
---
+++ AppServiceProxy/Configuration/YarpJsonConfigFileLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+using Yarp.ReverseProxy.Configuration;
+
+namespace AppServiceProxy.Configuration
+{
+    internal class YarpJsonConfigFileLoader : IConfigFileLoader
+    {
+        public string ConfigFileName => "yarp.json";
+
+        public (IReadOnlyList<RouteConfig>, IReadOnlyList<ClusterConfig>) LoadConfig(string contents)
+        {
+            try
+            {
+                var yarp = JsonSerializer.Deserialize<YarpJson>(contents);
+
+                return (yarp?.Routes ?? Array.Empty<RouteConfig>(), yarp?.Clusters ?? Array.Empty<ClusterConfig>());
+            }
+            catch
+            {
+                return (Array.Empty<RouteConfig>(), Array.Empty<ClusterConfig>());
+            }
+        }
+    }
+}
